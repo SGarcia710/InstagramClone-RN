8a79808f6dcd9d5c55fb4cd3bb06c0c407f9cabb
SGarcia710 <sgarcia710@outlook.com>
2021-05-06 20:11:29 -0500

    Login Screen finished

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -9,12 +9,83 @@ import {SearchScreen} from '../screens/Search';
 import {CreatePostScreen} from '../screens/CreatePost';
 import {NotificationsScreen} from '../screens/Notifications';
 import {ProfileScreen} from '../screens/Profile';
+import {Image, StyleSheet} from 'react-native';
 
 const RootTabs = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+  iconStyle: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+  },
+});
+
 const RootNavigator = () => {
   return (
-    <RootTabs.Navigator>
+    <RootTabs.Navigator
+      screenOptions={({route, navigation}) => {
+        return {
+          headerShown: false,
+          tabBarIcon: ({focused}) => {
+            switch (route.name) {
+              case 'Home':
+                return focused === true ? (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/home-filled.png')}
+                  />
+                ) : (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/home-outline.png')}
+                  />
+                );
+              case 'Search':
+                return focused === true ? (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/search-filled.png')}
+                  />
+                ) : (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/search-outline.png')}
+                  />
+                );
+              case 'CreatePost':
+                return (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/create-post-outline.png')}
+                  />
+                );
+              case 'Notifications':
+                return focused === true ? (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/notifications-filled.png')}
+                  />
+                ) : (
+                  <Image
+                    style={styles.iconStyle}
+                    source={require('../assets/icons/notifications-outline.png')}
+                  />
+                );
+              case 'Profile':
+                return (
+                  <Image
+                    style={[styles.iconStyle]}
+                    source={require('../assets/icons/profile.png')}
+                  />
+                );
+            }
+          },
+        };
+      }}
+      tabBarOptions={{
+        showLabel: false,
+      }}>
       <RootTabs.Screen name="Home" component={HomeScreen} />
       <RootTabs.Screen name="Search" component={SearchScreen} />
       <RootTabs.Screen name="CreatePost" component={CreatePostScreen} />
@@ -29,9 +100,12 @@ const Stack = createStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Root" component={RootNavigator} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Root" component={RootNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,24 +1,170 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-
-const LoginScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.text]}>Login</Text>
-    </View>
-  );
-};
+import {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
+    paddingHorizontal: 16,
   },
-  text: {
-    fontWeight: 'bold',
-    fontSize: 32,
+  form: {
+    marginTop: 39,
+    width: '100%',
+    alignItems: 'flex-end',
+    marginBottom: 30,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#FAFAFA',
+    borderWidth: 0.5,
+    borderColor: 'rgba(0,0,0,0.1)',
+    borderRadius: 5,
+    marginBottom: 12,
+    paddingHorizontal: 15,
+  },
+  forgotPasswordButtonText: {
+    color: '#3797EF',
+    fontSize: 12,
+  },
+  loginButtonContainer: {
+    backgroundColor: '#3797EF',
+    borderRadius: 5,
+    width: '100%',
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 37,
+  },
+  loginButtonText: {
+    color: 'white',
+  },
+  facebookLoginContainer: {
+    marginBottom: 41.5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  facebookLoginText: {
+    fontWeight: '700',
+    color: '#3797EF',
+    marginLeft: 10,
+  },
+  separatorContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    marginBottom: 41.5,
+  },
+  separator: {
+    borderWidth: 0.2,
+    flex: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+  },
+  separatorText: {
+    fontSize: 12,
+    color: 'rgba(0,0,0,0.4)',
+    marginHorizontal: 30.5,
+  },
+  dontHaveAccount: {
+    color: 'rgba(0,0,0,0.4)',
+  },
+  span: {
+    color: '#3797EF',
+  },
+  footer: {
+    borderTopWidth: 0.2,
+    borderTopColor: 'rgba(0,0,0,0.4)',
+    paddingTop: 32.5,
+    paddingBottom: 18.5,
+    alignItems: 'center',
+  },
+  footerText: {
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 12,
   },
 });
+
+const LoginScreen = ({navigation}) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{flexGrow: 1}}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <View style={styles.container}>
+          <Image source={require('../../assets/images/InstagramLogo.png')} />
+
+          <View style={styles.form}>
+            <TextInput
+              onChangeText={setEmail}
+              placeholder="Email"
+              style={[styles.input]}
+            />
+            <TextInput
+              onChangeText={setPassword}
+              secureTextEntry
+              placeholder="Password"
+              style={[styles.input, {marginBottom: 19}]}
+            />
+            <TouchableOpacity>
+              <Text style={styles.forgotPasswordButtonText}>
+                Forgot password?
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Root', {
+                screen: 'Home',
+              });
+            }}
+            disabled={email && password ? false : true}
+            style={[
+              styles.loginButtonContainer,
+              {
+                opacity: email && password ? 1 : 0.4,
+              },
+            ]}>
+            <Text style={styles.loginButtonText}>Log in</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.facebookLoginContainer}>
+            <Image source={require('../../assets/images/FacebookLogo.png')} />
+            <Text style={styles.facebookLoginText}>Log in with Facebook</Text>
+          </TouchableOpacity>
+
+          <View style={styles.separatorContainer}>
+            <View style={styles.separator} />
+            <Text style={styles.separatorText}>OR</Text>
+            <View style={styles.separator} />
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.dontHaveAccount}>
+              Don’t have an account? <Text style={styles.span}>Sign up.</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Instagram от Facebook</Text>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default LoginScreen;
